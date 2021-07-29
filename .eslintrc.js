1c7cbdf04013ae062477dc7f575029d7bda1c045
sk <seonk94@naver.com>
2021-07-29 21:14:13 +0900

    feat: fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'import/extensions': 0,
     'import/no-unresolved': 0,
     'no-use-before-define': 0,
+    'no-unused-vars': 0,
     'linebreak-style': 0,
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
   },
