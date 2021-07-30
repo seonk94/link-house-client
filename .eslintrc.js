b6fb3c7549c4259c428fbba863a6ffef0a2801d6
sk <seonk94@naver.com>
2021-07-30 22:55:05 +0900

    feat: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,10 +21,13 @@ module.exports = {
   ],
   rules: {
     'no-shadow': 0,
+    'no-alert': 0,
     'import/extensions': 0,
     'import/no-unresolved': 0,
     'no-use-before-define': 0,
+    'no-param-reassign': 0,
     'no-unused-vars': 0,
+    'no-undef': 0,
     'linebreak-style': 0,
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
   },
