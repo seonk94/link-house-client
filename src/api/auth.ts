a01f2354508656f5c13d087003b0f30103f40c33
sk <seonk94@naver.com>
2021-08-02 22:07:46 +0900

    refac: api type

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,7 +2,7 @@ import { AxiosInstance, AxiosPromise } from 'axios';
 import User from 'src/models/User';
 
 export default (axios: AxiosInstance) => ({
-  signup: (input: { name: string, email: string, password: string}) => axios.post('/api/auth/signup', { ...input }) as AxiosPromise<{ user: User, token: string }>,
-  signin: (input: { email: string, password: string }) => axios.post('/api/auth/signin', { ...input }) as AxiosPromise<{ user: User, token: string }>,
-  getMe: () => axios.get('/api/users/me') as AxiosPromise<{ user: User }>,
+  signup: (input: { name: string, email: string, password: string}): AxiosPromise<{ user: User, token: string }> => axios.post('/api/auth/signup', { ...input }),
+  signin: (input: { email: string, password: string }): AxiosPromise<{ user: User, token: string }> => axios.post('/api/auth/signin', { ...input }),
+  getMe: (): AxiosPromise<{ user: User }> => axios.get('/api/users/me'),
 });
--- src/api/link.ts
+++ src/api/link.ts
@@ -2,5 +2,6 @@ import { AxiosInstance, AxiosPromise } from 'axios';
 import Link from 'src/models/Link';
 
 export default (axios: AxiosInstance) => ({
-  getLinks: () => axios.get('/api/links') as AxiosPromise<Link[]>,
+  getLinks: (): AxiosPromise<Link[]> => axios.get('/api/links'),
+  getMetadata: (uri: string) => axios.post('/api/links/metadata', { uri }),
 });
