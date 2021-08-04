cc5809a4e4547268dc85e54f4ddfcb331a82d188
sk <seonk94@naver.com>
2021-08-04 22:15:34 +0900

    fix: links return type

--- src/api/link.ts
+++ src/api/link.ts
@@ -2,6 +2,6 @@ import { AxiosInstance, AxiosPromise } from 'axios';
 import Link from 'src/models/Link';
 
 export default (axios: AxiosInstance) => ({
-  getLinks: (): AxiosPromise<Link[]> => axios.get('/api/links'),
+  getLinks: (): AxiosPromise<{ links: Link[] }> => axios.get('/api/links'),
   getMetadata: (uri: string) => axios.post('/api/links/metadata', { uri }),
 });
